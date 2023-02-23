b434fb08af2ff8b86bd57c0f8fd07b02c425f2b0
Griffin White <52751516+december454@users.noreply.github.com>
2023-02-23 15:24:23 -0600

    Update Test-PID-Loop.ino

--- Contract Course - Spring 2023/Test-PID-Loop/Test-PID-Loop.ino
+++ Contract Course - Spring 2023/Test-PID-Loop/Test-PID-Loop.ino
@@ -3,14 +3,40 @@
 // Spring 2023 Contract Course
 // Engine Governor: PID Loop Control Test
 
-#include "Timer.h"            // Timer library.
-Timer timeElapsed(MILLIS);    // Timer object.
-Timer displayUpdate(MILLIS); 
-const int hallSensorPin = 2;  // Pin for hall effect sensor.
-const int numMagnets = 1;     // Number of magnets on the flywheel.
+const int hallSensorPin = 2;                  // Pin for hall effect sensor.
+const int numMagnets = 1;                     // Number of magnets on the flywheel.
+const int rpmPrecision = 2;                   // Acceptable discrepency between desired and actual rpm.
+const int in1 = 3, in2 = 4, in3 = 5, in4 = 6; // Pins for the stepper motor driver.
+
+
+
+
+
+double desiredRpm = 500;
+double Kp=2, Ki=5, Kd=1;        // PID variables.
+
+
+
+#include <Wire.h>               // LCD Library.
+#include <LiquidCrystal_I2C.h>  // LCD Library.
+#include <PID_v1.h>
+#include "Timer.h"              // Timer library.
+#include "CheapStepper.h"       // Stepper motor control library.
 
 double rpm = 0;               // Current RPM.
 int sensorActivations = 0;    // Number of times the hall sensor has been activated.
+
+double Output;
+
+Timer timeElapsed(MILLIS);    // Timer object for RPM calculations.
+Timer displayUpdate(MILLIS);  // Timer object for updating the LCD.
+PID thorttlePid(&rpm, &Output, &desiredRpm, Kp, Ki, Kd, DIRECT);
+CheapStepper stepperMotor(in1, in2, in3, in4);  // CheapStepper Object.
+LiquidCrystal_I2C lcd(0x27,20,4);
+
+
+
+
 
 void setup() {
   // Setting the SWITCH pinMode to INPUT_PULLUP.
