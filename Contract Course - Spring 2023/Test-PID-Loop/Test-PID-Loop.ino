afc59241c6f6a402ae574854b06384e9e4cf113a
Griffin White <52751516+december454@users.noreply.github.com>
2023-02-26 15:43:22 -0600

    Implemented custom PID code.
    Wrote simple PID control function, no longer using the "PID_v1" library.

--- Contract Course - Spring 2023/Test-PID-Loop/Test-PID-Loop.ino
+++ Contract Course - Spring 2023/Test-PID-Loop/Test-PID-Loop.ino
@@ -9,31 +9,37 @@ const int rpmPrecision = 2;                   // Acceptable discrepency between 
 const int in1 = 3, in2 = 4, in3 = 5, in4 = 6; // Pins for the stepper motor driver.
 const int updateInterval = 8;                 // Number of pulses between each calculation of the RPM.
 
+int pidP = 0, pidI = 0, pidD = 0;
+
+
+
+
+
 double desiredRpm = 500;      // Desired RPM.
 double rpm = 0;               // Current RPM.
 double rpmDiff = 0;           // Difference between the current RPM and desired RPM.
+double rpmDiffPrev = 0; 
 int sensorActivations = 0;    // Number of times the hall sensor has been activated.
 int stepsRemaining = 0;       // Number of stepper motor steps remaining.
 bool throttleRpmUpdated = false;
 boolean directionFlag = false; 
 
-// double Kp=2, Ki=5, Kd=1;        // PID variables.
+ double Kp=1.1, Ki=.002, Kd=0;        // PID variables.
 // double Kp=2, Ki=20, Kd=10;        // PID variables.
 // double Kp=15, Ki=1, Kd=1; 
 // double Kp=.0, Ki=0, Kd=2; 
-double Kp=1, Ki=.00001, Kd=.000001; 
-double pidOutput = 0;       
+// double Kp=.000005, Ki=1, Kd=1; 
+// double Kp=.0002, Ki=.00055, Kd=.0001;     
 
 #include <Wire.h>               // LCD library.
 #include <LiquidCrystal_I2C.h>  // LCD library.
-#include <PID_v1.h>             // PID library.
 #include "Timer.h"              // Timer library.
 #include "CheapStepper.h"       // Stepper motor control library.
 
 Timer timeElapsed(MILLIS);      // Timer object for RPM calculations.
 Timer displayUpdate(MILLIS);    // Timer object for updating the LCD.
+Timer pidTimeElapsed(MILLIS);
 
-PID throttlePid(&rpm, &pidOutput, &desiredRpm, Kp, Ki, Kd, DIRECT);
 CheapStepper stepperMotor(in1, in2, in3, in4);  // CheapStepper Object.
 LiquidCrystal_I2C lcd(0x27,20,4);               // LCD Object.
 
@@ -42,8 +48,7 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(hallSensorPin),countPulse,FALLING); // Attctching an interrupt to the hallSensorPin.
   Serial.begin(9600);                   // Initializing serial output.
   displayUpdate.start();                // Starting the display update timer.
-  throttlePid.SetMode(AUTOMATIC);       // Starting the throttlePid control.
-  throttlePid.SetOutputLimits(0,200);   // Settingthe output limits for the throttlePid.
+  pidTimeElapsed.start();
 }
 
 void loop() {
@@ -56,13 +61,12 @@ void loop() {
     adjustThrottle();
   else
     stepperMotor.stop();
-  
 
-  if(displayUpdate.read() >= 1000){
+  if(displayUpdate.read() >= 200){
     Serial.print("RPM: ");
     Serial.println(rpm);
-    Serial.print("pidOutput: ");
-    Serial.println(pidOutput);
+    //Serial.print("Steps Remaining: ");
+    // Serial.println(stepsRemaining);
     displayUpdate.start();    
   }   
 }
@@ -78,12 +82,10 @@ void calcRpm(){
   // If the RPM is above the desired value.
   if (rpmDiff < 0){
     directionFlag = true;                         // Setting the stepper motor direction to clockwise (increase throttle).
-    throttlePid.SetControllerDirection(REVERSE);  // Setting the throttle PID loop for reverse (above desired speed) operation.
   }
   // Else the RPM is below the desired value.
   else{
     directionFlag = false;                        // Setting the stepper motor direction to counter-clockwise (decrease throttle).
-    throttlePid.SetControllerDirection(DIRECT);   // Setting the throttle PID loop for direct (above desired speed) operation.   
   }      
   throttleRpmUpdated = true;  // Flagging that the RPM was just updated.
 }
@@ -92,8 +94,7 @@ void calcRpm(){
 void adjustThrottle(){
   // If the RPM has been updated since the last method call, recalculate the number of stepper motor steps.
   if (throttleRpmUpdated){
-    throttlePid.Compute();            // Compute the output of the throttle PID loop (the corrections to be made).
-    stepsRemaining = abs(pidOutput);  // Set stepsRemaining equal to the output of the PID loop.
+    stepsRemaining = calculatePid();  // Set stepsRemaining equal to the output of the PID loop.
     throttleRpmUpdated = false;       // Flagging that the new RPM has been acknowldged.
   }
 
@@ -108,4 +109,20 @@ void countPulse(){
       
   sensorActivations++;      // Increment the number of sensorActivations.    
   
+}
+
+int calculatePid(){
+  
+  pidP = Kp * rpmDiff;
+  pidD = Kd * rpmDiff - ((rpmDiffPrev) / pidTimeElapsed.read());
+  
+
+  if (abs(rpmDiff) > rpmPrecision)
+    pidI += pidI + (Ki * rpmDiff);
+  else
+    pidI = 0;
+  
+  pidTimeElapsed.start();
+
+  return abs(pidP + pidI + pidD);    
 }
