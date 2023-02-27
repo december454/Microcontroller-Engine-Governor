687b6302b6933726952a80eafb4eb102d73b0bc6
Griffin White <52751516+december454@users.noreply.github.com>
2023-02-26 20:24:20 -0600

    Began writing final program.

---
+++ Contract Course - Spring 2023/Final-Program/Final-Program.ino
@@ -0,0 +1,14 @@
+// Griffin White
+// 2-26-2023
+// Spring 2023 Contract Course
+// Engine Governor: Final Program - Version 1
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
